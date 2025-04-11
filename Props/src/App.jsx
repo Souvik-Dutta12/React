f3cd63058a33c7ce94adee17b5e2d69b5d36d95c
Souvik Dutta <souvikdutta5122005@gmail.com>
2025-04-11 19:24:35 +0530

    Complete Props

--- Props/src/App.jsx
+++ Props/src/App.jsx
@@ -2,32 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './Components/Card.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
+  let myObj = {
+    username: "hitesh",
+    age: 21
+  }
+  let newArr = [1, 2, 3]
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1 className='text-7xl text-white mt-40 ml-70'>Chai aur React -- 03 Props --</h1>
+      <div className="cards flex gap-4 mt-10 ml-70 flex-wrap">
+      <Card username="chaiaurcode" btnText="click me" />
+      <Card username="hitesh" />    
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- Props/src/Components/Card.jsx
+++ Props/src/Components/Card.jsx
@@ -11,12 +11,12 @@ function Card({username, btnText="visit me"}) {
   />
   <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
   <div className="absolute bottom-4 left-4 text-left">
-    <h1 className="text-lg font-semibold text-white">{username}</h1>
+    <h1 className="text-lg font-semibold text-emerald-500">{username}</h1>
     <p className="mt-2 text-sm text-gray-300">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
       debitis?
     </p>
-    <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+    <button className="mt-2 inline-flex bg-blue-500 rounded-md px-5 py-1 cursor-pointer items-center text-sm font-semibold text-white hover:bg-blue-600">
       {btnText } →
     </button>
   </div>
