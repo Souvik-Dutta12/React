891c277211a3f1391b34921214c3daf010b5e3d1
Souvik Dutta <souvikdutta5122005@gmail.com>
2025-04-15 20:03:55 +0530

    Router

--- Router/src/main.jsx
+++ Router/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
